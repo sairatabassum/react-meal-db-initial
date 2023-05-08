027ea75f077d2bbddeceb65b8178877c21965728
Saira Tabassum <sairatabassum386@gmail.com>
2023-05-08 12:21:42 +0600

    Search food

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Restaurant from './components/Restaurant/Restaurant';
-import Header from './components/Header/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Restaurant from "./components/Restaurant/Restaurant";
+import Header from "./components/Header/Header";
+import SearchFood from "./components/SearchFood/SearchFood";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <SearchFood></SearchFood>
       <Restaurant></Restaurant>
     </div>
   );
